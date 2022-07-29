dd10c72476c837b05cf0cc378699db67cdeeb244
cuong.pham <cuong.pham@sotatek.com>
2022-07-29 10:51:13 +0700

    #update socket

--- app/http/ws/index.ts
+++ app/http/ws/index.ts
@@ -1,47 +1,50 @@
 import {io} from "socket.io-client";
+const socket = io("http://localhost:3000");
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJmaXJzdE5hbWUiOm51bGwsImxhc3ROYW1lIjpudWxsLCJlbWFpbCI6ImExQGdtYWlsLmNvbSIsInJvbGUiOiJ1c2VyIn0sImlhdCI6MTY0MzcwOTQ4MywiZXhwIjoxNjQzNzk1ODgzfQ.5m0F_lrjHrFy7ZCSDDqw9SG55l33YyT0hp0yqvDLjA0"
 
-const socket = io("http://localhost:4000", {
-  query: { token },
-  transportOptions: {
-    polling: {
-      extraHeaders: {
-        Authorization: `${token}`
-      }
-    }
-  }
-});
 
-const chatSocket = io("http://localhost:3001/chat", {
-  path: '/websockets',
-  query: { token },
-  // extraHeaders: {
-  //   Authorization: `${token}`
-  // },
-  transportOptions: {
-    polling: {
-      extraHeaders: {
-        Authorization: `${token}`
-      }
-    }
-  }
-  // transports: ['polling', 'websocket']
-});
+// const socket = io("http://localhost:3000", {
+//   query: { token },
+//   transportOptions: {
+//     polling: {
+//       extraHeaders: {
+//         Authorization: `${token}`
+//       }
+//     }
+//   }
+// });
 
-const alertSocket = io("http://localhost:3001/alert", {
-  path: '/websockets',
-  query: { token },
-  // extraHeaders: {
-  //   Authorization: `${token}`
-  // },
-  transportOptions: {
-    polling: {
-      extraHeaders: {
-        Authorization: `${token}`
-      }
-    }
-  }
-  // transports: ['polling', 'websocket']
-});
+// const chatSocket = io("http://localhost:3001/chat", {
+//   path: '/websockets',
+//   query: { token },
+//   // extraHeaders: {
+//   //   Authorization: `${token}`
+//   // },
+//   transportOptions: {
+//     polling: {
+//       extraHeaders: {
+//         Authorization: `${token}`
+//       }
+//     }
+//   }
+//   // transports: ['polling', 'websocket']
+// });
+//
+// const alertSocket = io("http://localhost:3001/alert", {
+//   path: '/websockets',
+//   query: { token },
+//   // extraHeaders: {
+//   //   Authorization: `${token}`
+//   // },
+//   transportOptions: {
+//     polling: {
+//       extraHeaders: {
+//         Authorization: `${token}`
+//       }
+//     }
+//   }
+//   // transports: ['polling', 'websocket']
+// });
 
-export {socket, chatSocket, alertSocket}
+// export {socket, chatSocket, alertSocket}
+export {socket}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,65 @@
 import type {NextPage} from 'next'
 import styles from '../styles/Home.module.css'
-import {socket} from '../app/http/ws'
 import {useEffect} from "react";
+import {io} from "socket.io-client";
+import WebSocket from 'ws';
+// const socket = new WebSocket("ws+unix://localhost:3333/", {
+//   perMessageDeflate: false
+// })
+
+
+const socket = io("http://localhost:3333", {
+  // extraHeaders: {
+  //   Host: 'localhost:3333',
+  //   Origin: 'ws://localhost:3000',
+  //   Referer: 'ws://localhost:3000/'
+  // },
+  // secure:true,
+  // forceNew: true,
+  transports: ["websocket", "polling"],
+  // transportOptions: {
+  //   polling: {
+  //     extraHeaders: {
+  //
+  //     }
+  //   }
+  // }
+    // autoConnect: false,
+  upgrade: true,
+  protocols: '-'
+});
+
+
+console.log({socket})
+
 const Home: NextPage = () => {
   useEffect(() => {
     socket.on('chatToClient', (d) => {
+      console.log({d})
       alert('chatToClient' + d)
+    })
+
+    socket.on('odds_change', (d) => {
+      console.log({d})
+      alert('chatToClient' + d)
+    })
+
+    socket.on('joinedRoom', (d) => {
+      alert('joinedRoom' + d)
+      // socket.send(
+      //   JSON.stringify({
+      //     event: 'events',
+      //     data: 'test',
+      //   }),
+      // );
+
     })
   }, [])
 
   return (
     <div className={styles.container}>
       <button onClick={() => {
-        socket.emit('chatToServer', '2383333')
+        socket.emit('joinRoom', 'sr:match:32690467')
       }}>A</button>
     </div>
   )
